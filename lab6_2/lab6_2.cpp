dfc504b0e8776b97e8aaa25200a7a245d3ca76b2
Newstar12321 <tazeev.zhenya@mail.ru>
2023-12-04 10:37:30 +0700

    Добавьте файлы проекта.

---
+++ lab6_2/lab6_2.cpp
@@ -0,0 +1,66 @@
+﻿/*
+2.	Дана квадратная матрица порядка M. Обнулить элементы матрицы,
+лежащие выше побочной диагонали. Условный оператор не использовать.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	size_t m;
+	cout << "Введите порядок матрицы: ";
+	cin >> m;
+
+	if (m == 0)
+	{
+		cerr << "Ошибка: порядок матрицы не может быть равен нулю";
+		return 1;
+	}
+
+	int** array;
+	array = new int* [m];
+
+	for (size_t i = 0; i < m; i++)
+	{
+		array[i] = new int[m];
+	}
+
+	cout << "Введите элементы матрицы:" << endl;
+	for (size_t i = 0; i < m; i++)
+	{
+		for (size_t j = 0; j < m; j++)
+		{
+			cin >> array[i][j];
+		}
+	}
+
+	for (size_t i = 0, k = m - 1; i < m; i++, k--)
+	{
+		for (size_t j = 0; j < k; j++)
+		{
+			array[i][j] = 0;
+		}
+	}
+
+	cout << "Полученная матрица: " << endl;
+	for (size_t i = 0; i < m; i++)
+	{
+		for (size_t j = 0; j < m; j++)
+		{
+			cout << array[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	for (size_t i = 0; i < m; i++)
+	{
+		delete[] array[i];
+	}
+	delete[] array;
+
+}
+
